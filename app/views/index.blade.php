b80c66fa6ff75456b23b0210bb5fbaaf836a9a75
chaos29092 <wo94yun>
2014-10-28 15:21:36 +0800

    Most of the content done

--- app/views/index.blade.php
+++ app/views/index.blade.php
@@ -33,13 +33,12 @@
 
       <div id="logo-container">
       <ul class="list check">
-                 <li>22-year manufacture experience</li>
-                 <li>10-year export experience</li>
-                 <li>Optimized container solution</li>
-                 <li>Questionnaire assistance</li>
-                 <li>Smallest mobile model: 20t/h</li>
-                 <li>Smallest fixed model: 40t/h</li>
-                 <li>Loan facility</li>
+                 <li>22 лет опыта производства</li>
+                 <li>10 лет опыта экспорта</li>
+                 <li>Оптимизация программы </li>
+                 <li>контейнерных перевозок</li>
+                 <li>Конкуренто способность цены и качества</li>
+                 <li>Опросной лист по закупк</li>
                </ul>
 
       </div>
@@ -66,31 +65,11 @@
         </div>
 
     <div class="element home clearfix col2-3 services">
-    <h3>Mobile Asphalt Mixing Plant Parameters</h3>
-        <ul class="list check">
-            <li>DHB20 &nbsp;<strong>(China smallest)</strong>, &nbsp;20t/h, &nbsp;2×40HQ, &nbsp;<strong>FOB &nbsp;QD: &nbsp;USD 50, &nbsp;000</strong>.<br>
-               <small>Oil burner, &nbsp;15t direct heat bitumen tank, &nbsp;cyclone dust collector, &nbsp;2.5m³ silo of final product</small></li>
-            <li>DHB40, &nbsp;40t/h, &nbsp;3×40HQ+1×20GP, &nbsp;<strong>FOB QD: &nbsp;USD 87, &nbsp;000</strong>.<br>
-                <small>Oil burner, &nbsp;20t direct heat bitumen tank,&nbsp; water film dust filter, &nbsp;2.5m³ silo of final product</small></li>
-            <li>DHB60 <strong>&nbsp;(Hot model)</strong>, &nbsp;60t/h, &nbsp;4×40HQ+1×20GP, &nbsp;<strong>FOB QD: &nbsp;USD 104, &nbsp;000</strong>.<br>
-                <small>Oil burner, &nbsp;30t direct heat bitumen tank, &nbsp;water film dust filter, &nbsp;3.5m³ silo of final product</small></li>
-            <li>DHB80 <strong>&nbsp;(Hot model)</strong>, &nbsp;80t/h, &nbsp;5×40HQ+1×20GP, &nbsp;<strong>FOB QD: USD 120, &nbsp;000</strong>.<br>
-            <small>Oil burner, &nbsp;30t direct heat bitumen tank, &nbsp;water film dust filter, &nbsp;4.2m³ silo of final product</small></li>
-          </ul>
+               <div class="images"><img src="{{asset('img/canshu_1.jpg')}}" alt="" /></div>
     </div>
 
     <div class="element home clearfix col2-3 services">
-    <h3>Stationary Asphalt Mixing Plant Parameters</h3>
-      <ul class="list check">
-          <li>LB500-1, &nbsp;40t/h, &nbsp;7×40HQ, &nbsp;<strong>FOB QD: &nbsp;USD 129, &nbsp;000</strong>.<br/>
-         <small>Oil burner, &nbsp;20t direct heat bitumen tank, water film dust filter</small></li>
-          <li>LB800-2, &nbsp;64t/h, &nbsp;8×40HQ, &nbsp;<strong>FOB QD: &nbsp;USD 185, &nbsp;000</strong>.<br/>
-          <small>Oil burner, &nbsp;30t bitumen tank, &nbsp;300,&nbsp;000 Kcal thermal oil furnace, &nbsp;water film dust filter.</small></li>
-          <li>LB1000-2, &nbsp;80t/h, &nbsp;8×40HQ, &nbsp;<strong>FOB QD: &nbsp;USD 220, &nbsp;000</strong>.<br/>
-          <small>Oil burner, &nbsp;40t bitumen tank, &nbsp;300,&nbsp;000 Kcal thermal oil furnace, &nbsp;water film dust collector</small></li>
-          <li>LB1000-4, &nbsp;80t/h, &nbsp;10×40HQ, &nbsp;<strong>FOB QD: &nbsp;USD 280, &nbsp;000</strong>.<br/>
-          <small>Oil burner, &nbsp;40t bitumen tank, &nbsp;300,&nbsp;000 Kcal thermal oil furnace, &nbsp;bag dust collector</small></li>
-      </ul>
+               <div class="images"><img src="{{asset('img/canshu_2.jpg')}}" alt="" /></div>
     </div>
 
         <div class="element home clearfix col1-3 borders portfolio overflow">
@@ -99,29 +78,9 @@
              </div>
         </div>
 
-    <div class="element home clearfix col2-3 services">
-    <h3>Stationary Asphalt Mixing Plant Parameters</h3>
-          <ul class="list check">
-              <li>LB1500-2, &nbsp;120t/h, &nbsp;9×40HQ,&nbsp;<strong>FOB QD: &nbsp;USD 319, &nbsp;000</strong>.<br/>
-              <small>Oil burner, &nbsp;2×40t bitumen tank, &nbsp;400,&nbsp;000 Kcal thermal oil furnace, &nbsp;water film dust filter</small></li>
-              <li>LB1500-4, &nbsp;120t/h, &nbsp;11×40HQ，<strong>FOB QD: &nbsp;USD 345, &nbsp;000</strong>.<br/>
-              <small>Oil burner, &nbsp;2×40t bitumen tank, &nbsp;400,&nbsp;000 Kcal thermal oil furnace, &nbsp;bag dust collector.</small></li>
-              <li>LB2000, &nbsp;160t/h, &nbsp;16×40HQ, &nbsp;<strong>FOB QD: &nbsp;USD 527, &nbsp;000</strong>.<br/>
-             <small>Oil burner, &nbsp;2×40t bitumen tank, &nbsp;500,000 Kcal thermal oil furnace, &nbsp;Bag dust collector, &nbsp;30t silo of final product, &nbsp;Siemens element</small></li>
-
-          </ul>
-        </div>
-
-        <div class="element home clearfix col1-3 borders portfolio overflow">
-            <div class="movable-content">
-                <div class="images"><img src="{{asset('img/asphalt-mixing-plant-3.jpg')}}" alt="" /></div>
-             </div>
-        </div>
-
     <div class="element home clearfix col1-3 border services">
-      <h3>Questionnaire</h3>
-      <p><small>We’re here to ensure you get the right machine for your yard with the support to keep it running at peak performance levels.</small>
-      <small>Please <a href="">ask for questionnaire</a> from us.</small><br/><br/></p>
+      <h3>Опросный лист</h3>
+      <p>Для обеспечения точного выбора конфигурации, пожалуйста, свяжитесь с нами чтобы получить опросный лист.<br/><br/></p>
 
     </div>
 
@@ -219,16 +178,16 @@
 
 <div>
 <img src="{{asset('img/hanvyman.jpg')}}" alt="Account Manager"/>
-<p>My name is Richard Han,<br/>
-sales manager of Zhengzhou Hanvy Machinery & Equipment Co., Ltd.</p><br/>
-<p>Thanks for visit our website.</p><br/>
-<p>If you need talk now,please add my skype: vp05hanvyzj</p><br/>
-<p>More surprise is waiting for you, <br/>service we can supply is far more than products itself.</p>
+<p>Здравствуйте! Меня зовут Дима,<br/>
+ я менеджер по продажам компании HANVY.</p><br/>
+<p>Добро пожаловать на наш веб-сайт!</p><br/>
+<p>Пожалуйста, добавьте мой Skype чтобы получить <br/>
+ подробные информацию: vp20hanvyzj</p><br/>
 </div>
 </div>
 
 <div class="alignright">
-<h3>Contact Us</h3>
+<h3>Свяжитесь с нами</h3>
 {{ Form::open(array('url' => 'mailPost')) }}
     <div>
     {{Form::label('email', 'E-Mail(*)  :')}}
